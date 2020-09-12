09bf5e425456c7313c79a8e8fa8b15e893a74a88
Thuy Dao <thuydao@kms-technology.com>
2020-09-12 16:18:19 +0700

    Update config

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'arrow-body-style': [2, 'as-needed'],
+    'class-methods-use-this': 0,
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 2,
+    'import/no-webpack-loader-syntax': 0,
+    'import/prefer-default-export': 0,
+    indent: [
+      2,
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'comma-dangle': 0,
+    'consistent-return': 0,
+    'function-paren-newline': ['error', 'never'],
+    'implicit-arrow-linebreak': ['off'],
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'no-shadow': 0,
+    'no-plusplus': 0,
+    'no-unused-expressions': 0,
+    'max-len': 0,
+    'newline-per-chained-call': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 1,
+    'no-unused-vars': 2,
+    'no-use-before-define': 0,
+    'prefer-template': 2,
+  },
+};
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ connect();
 APP.use(bodyParser.json());
 APP.use(bodyParser.urlencoded({ extended: false }));
 
-APP.use('/', router);
+APP.use('/api', router);
 
 APP.listen(PORT, (req, res) => {
     console.log("Server is running on port: " + PORT);
